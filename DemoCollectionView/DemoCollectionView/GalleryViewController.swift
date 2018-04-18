35696702f8121dec8a2045ad5b01a39616d36d62
Rajesh <rajeshsahani071@gmail.com>
2018-04-18 12:11:52 +0530

    Rename ViewController to GalleryViewController

---
+++ DemoCollectionView/DemoCollectionView/GalleryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoCollectionView
+//
+//  Created by Test User 1 on 17/04/18.
+//  Copyright © 2018 Capgemini. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class GalleryViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+}
+
+}
