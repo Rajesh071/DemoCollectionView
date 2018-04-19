feca24ad6d40bef2874e3e59f36e5e02e870cace
Rajesh <rajeshsahani071@gmail.com>
2018-04-19 12:15:29 +0530

    Removed pod integration

---
+++ DemoCollectionView/DemoCollectionView/GalleryCell.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryCell.swift
+//  DemoCollectionView
+//
+//  Created by Test User 1 on 18/04/18.
+//  Copyright © 2018 Capgemini. All rights reserved.
+//
+
+import UIKit
+
+class GalleryCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
